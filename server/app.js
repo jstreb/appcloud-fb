cd3c31f495181a805c744a108c377d391375dc7e
Jesse Streb <jstreb@brightcove.com>
2012-06-18 11:06:26 -0400

    setting the port to the one heroku assings in prod

--- server/app.js
+++ server/app.js
@@ -8,6 +8,8 @@ var express = require('express')
 
 var app = module.exports = express.createServer();
 
+var port = process.env.PORT || 3000;
+
 // Configuration
 
 app.configure(function(){
@@ -33,6 +35,6 @@ app.get('/', routes.index);
 
 app.get('/login', routes.login );
 
-app.listen(3000, function(){
+app.listen(port, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
